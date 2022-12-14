e2855070b1f796646a17424ff12b700c736927c4
DESKTOP-FO3JRPK\javier <javiergom310@gmail.com>
2022-12-14 10:06:00 +0100

    Agregar archivos de proyecto.

---
+++ pruebaONpost/Modelo/Persona.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pruebaONpost.Modelo
+{
+    public class Persona
+    {
+
+        [BindProperty]
+        public string nombre { get; set; }
+
+        [BindProperty]
+        public string apellidos { get; set; }
+    }
+}
---
+++ pruebaONpost/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using pruebaONpost.Modelo;
+
+namespace pruebaONpost.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string name { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostSubmit(Persona persona)
+        {
+
+            this.name = string.Format("nombre: " + persona.nombre + " " + persona.apellidos);
+
+        }
+    }
+}
